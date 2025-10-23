38c725a3b57ae3d4eae1cf8d6adf62d259c803fb
zhmaylovv <zhmaylovv@gmail.com>
2025-10-23 15:00:34 +0700

    histogramPercent, findMine codewars

--- src/main/java/org/example/CodeWars.java
+++ src/main/java/org/example/CodeWars.java
@@ -5,7 +5,7 @@ public class CodeWars {
     public static String histogram(final int results[]) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = results.length - 1; i >= 0; i--) {
-            stringBuilder.append(i+1)
+            stringBuilder.append(i + 1)
                     .append("|")
                     .append("#".repeat(Math.max(0, results[i])));
             if (results[i] > 0) {
@@ -15,5 +15,33 @@ public class CodeWars {
         }
         return stringBuilder.toString();
     }
+
+    public static String histogramPercent(final int results[]) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int sum = 0;
+        for (int num : results) sum = sum + num;
+        sum = sum == 0 ? 1 : sum;
+        for (int i = results.length - 1; i >= 0; i--) {
+            stringBuilder.append(i + 1)
+                    .append("|")
+                    .append("█".repeat(results[i] * 100 / sum * 50 / 100));
+            if (results[i] > 0) {
+                stringBuilder.append(" ").append(results[i] * 100 / sum).append("%");
+            }
+            stringBuilder.append("\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    public static int[] findMine(int[][] field){
+        int[] result = new int[2];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                if (field[i][j] == 1) result = new int[]{i, j};
+            }
+        }
+        return result;
+    }
+
 
 }
--- src/test/java/CodeWarsTest.java
+++ src/test/java/CodeWarsTest.java
@@ -3,12 +3,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.example.CodeWars.findMine;
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CodeWarsTest {
     @Test
-    public void basic() {
+    public void histogram() {
         final String expected =
                 "6|##### 5\n"+
                         "5|\n"+
@@ -17,5 +19,37 @@ public class CodeWarsTest {
                         "2|### 3\n"+
                         "1|####### 7\n";
         assertEquals(expected, CodeWars.histogram(new int[]{7,3,10,1,0,5}));
+    }
+
+    @Test
+    public void example() {
+        final String expected =
+                "6|██ 5%\n"+
+                        "5|\n"+
+                        "4|███████ 15%\n"+
+                        "3|███████████████████████████████████ 70%\n"+
+                        "2|█ 3%\n"+
+                        "1|███ 7%\n";
+        assertEquals(expected, CodeWars.histogramPercent(new int[]{7,3,70,15,0,5}));
+    }
+
+    @Test
+    public void histogramPercent() {
+        final String expected =
+                "6|██ 5%\n"+
+                        "5|\n"+
+                        "4|███████ 15%\n"+
+                        "3|███████████████████████████████████ 70%\n"+
+                        "2|█ 3%\n"+
+                        "1|███ 7%\n";
+        assertEquals(expected, CodeWars.histogramPercent(new int[]{0,0,0,0,0,0}));
+    }
+
+    @Test()
+    public void basicTestCases() {
+        assertArrayEquals(new int[] {0,0}, findMine( new int[][] { {1, 0}, {0, 0} } ));
+        assertArrayEquals(new int[] {0,0}, findMine( new int[][] { {1, 0, 0}, {0, 0, 0}, {0, 0, 0} } ));
+        assertArrayEquals(new int[] {2,2}, findMine( new int[][] { {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 1, 0}, {0, 0, 0, 0} } ));
+
     }
 }
